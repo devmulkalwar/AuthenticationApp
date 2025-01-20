9a70c9d1191bdcb84a55e4658b0e24a8c0380866
devmulkalwar <devmulkalwar95@gmail.com>
2025-01-20 19:53:56 +0530

    environment variable setup done

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/dbConnection.js";
 import authRoutes from "./routes/authRoutes.js";
-import path from "path";
 
 // Load environment variables
 dotenv.config();
@@ -13,16 +12,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Resolve the current directory
-const __dirname = path.resolve();
-
 // Database connection
 connectDB();
 
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", // Use environment variable for frontend URL
     credentials: true, // Allow cookies
   })
 );
@@ -32,14 +28,6 @@ app.use(cookieParser()); // Parse cookies
 
 // Routes
 app.use("/api/auth", authRoutes); // Authentication routes
-
-// Serve static files in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
-  });
-}
 
 // Error handling middleware
 app.use((err, req, res, next) => {
--- frontend/src/contexts/GlobalContext.jsx
+++ frontend/src/contexts/GlobalContext.jsx
@@ -4,11 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { GlobalContext } from "@/hooks/useGlobalContext";
 
-const SERVER_URL =
-  import.meta.env.VITE_MODE === "development"
-    ? "http://localhost:3000/api/auth"
-    : "/api/auth";
-
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 // Create a provider component
 export const ContextProvider = ({ children }) => {
   const navigate = useNavigate();
